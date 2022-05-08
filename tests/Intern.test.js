976738bb2972ea805dda3dd4db4f3adddf2121bf
msamm <msamm93@gmail.com>
2022-05-08 11:53:11 -0500

    Intern test and js

--- tests/Intern.test.js
+++ tests/Intern.test.js
@@ -3,3 +3,18 @@ school
 getSchool()
 getRole() Overridden to return Intern
 */
+
+const Intern = require('../lib/Intern');
+
+const intern = new Intern('Iris', '33333', 'iris@employer.com', 'Purdue University');
+
+test('creates intern object', () => {
+    expect(intern.name).toBe('Iris');
+    expect(intern.id).toBe('33333');
+    expect(intern.email).toBe('iris@employer.com');
+    expect(intern.school).toBe('Purdue University');
+});
+
+test('get intern school', () => {
+    expect(intern.getSchool()).toEqual(expect.stringContaining('Purdue University'));
+});
