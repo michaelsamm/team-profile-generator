73f3c23d52e78b00e1249053b6666aa503b955e4
msamm <msamm93@gmail.com>
2022-05-08 11:44:33 -0500

    Engineer tests and js

--- tests/Engineer.test.js
+++ tests/Engineer.test.js
@@ -3,3 +3,18 @@ github GitHub username
 getGithub()
 getRole() Overridden to return Engineer
 */
+
+const Engineer = require('../lib/Engineer');
+
+const engineer = new Engineer('Ed', '22222', 'ed@employer.com', 'edhub');
+
+test('creates Engineer object', () => {
+    expect(engineer.name).toBe('Ed');
+    expect(engineer.id).toBe('22222');
+    expect(engineer.email).toBe('ed@employer.com');
+    expect(engineer.githubUsername).toBe('edhub');
+});
+
+test('gets engineer github username', () => {
+    expect(engineer.getGithub()).toEqual(expect.stringContaining('edhub'));
+});
